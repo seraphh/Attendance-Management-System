28e220923aa26f20486504ebec63a8df8449ce98
seraphh <82749337+seraphh@users.noreply.github.com>
2023-12-14 00:16:35 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/attendancemanagementsystem/Login.java
@@ -0,0 +1,12 @@
+package com.mycompany.attendancemanagementsystem;
+
+import com.mycompany.attendancemanagementsystem.Database;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Login Page");
+        
+        
+        
+    }
+}
